96e790559bbc701560ed6aed9283ed37419a3c15
unknown <carlosdanielrod@gmail.com>
2022-10-05 20:37:35 -0500

    implemented makePassword prototype

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from "react"
+
 export const LoginForm = () => {
+
+    const makePassword = (username) => {
+        let usernameArray = username.split('')
+        let passwordArray = []
+        usernameArray.forEach(char => {
+            if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+                return null
+            } else {
+                passwordArray.push(char)
+            }
+        })
+        console.log(passwordArray)
+        let newPassword = passwordArray.join('')
+        console.log(newPassword)
+    }
+    useEffect(() => {
+        makePassword('hola')
+    }, [])
+
     return (
         <>
             <h1>login form</h1>
