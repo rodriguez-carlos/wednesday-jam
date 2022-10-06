de6d45adfcee89e0f6eae1a5bd867f07f367de04
unknown <carlosdanielrod@gmail.com>
2022-10-06 16:58:26 -0500

    added font awesome dependencies

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,16 @@
 import { useEffect } from "react"
 import { getWeatherInfo } from "../api"
 import { useTheme } from "@emotion/react"
-import { ActionButton, MainHeading, Text } from "./ContentSections/common"
+import { ActionButton, MainHeading, Section, Text } from "./ContentSections/common"
+import styled from "@emotion/styled"
+
+const LoginFormSection = styled(Section)`
+  display: flex;  
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
 
 export const LoginForm = () => {
     const theme = useTheme();
@@ -16,10 +25,10 @@ export const LoginForm = () => {
     }, [])
 
     return (
-        <>
+        <LoginFormSection>
             <MainHeading fontSize='20px'>Volante QuickConnect</MainHeading>
             <Text fontSize='12px' color={theme.colors.gray}>Please login to your platform</Text>
             <ActionButton bgColor={theme.colors.blue} color={theme.colors.white} fontSize="20px">LOGIN</ActionButton>
-        </>
+        </LoginFormSection>
     )
 }
